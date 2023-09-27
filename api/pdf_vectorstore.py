29e9a27f50453719b4c55478c15a204d4adb3f9a
Yoshifumi12321 <yuzu12321@gmail.com>
2023-09-27 14:24:53 +0900

    PDFの内容を回答してくれるアプリの追加

---
+++ api/pdf_vectorstore.py
@@ -0,0 +1,90 @@
+import os
+import shutil
+
+import openai
+import chromadb
+import langchain
+from langchain.llms import OpenAI
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+
+from pydantic import BaseModel  # リクエストbodyを定義するために必要
+
+# API用
+from fastapi import FastAPI, File, UploadFile
+
+import tempfile
+
+api = FastAPI()
+
+os.environ["OPENAI_API_KEY"] = '<input_your_api_key>'
+model_name = "text-davinci-002"
+
+# ######################################
+# PDFを読み込みベクトル化して保存するためのAPI
+# usage: curl -X POST -F 'file=@./docs/kantan_service.pdf' http://127.0.0.1:8000/pdf
+@api.post("/pdf")
+async def post(file: UploadFile = File(...)):
+  if file:
+    # アップロードされたファイルをローカルにコピー
+    pdf_filename = file.filename
+    pdf_fileobj = file.file
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+      print(tmp_file.name)
+      pdf_upload_dir = open(tmp_file.name,'wb+')
+      # アップロードされたファイルをローカルにコピー
+      shutil.copyfileobj(pdf_fileobj, pdf_upload_dir)
+      # ベクトル化して保存
+      to_vectorstore(pdf_upload_dir.name)
+      pdf_upload_dir.close()
+    return {"message": "PDFが正常に読み込まれました"}
+  return {"Error": "アップロードファイルが見つかりません。"}
+
+# ベクトル化して保存する関数
+def to_vectorstore(pdf_file_path):
+  loader = PyPDFLoader(pdf_file_path)
+  pages = loader.load_and_split()
+
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+  llm = OpenAI(temperature=0,model_name=model_name,
+    max_tokens=512,
+    frequency_penalty=0.02
+  )
+  # save to disk
+  embeddings = OpenAIEmbeddings()
+  vectorstore = Chroma.from_documents(pages, embedding=embeddings, persist_directory="./chroma_db")
+  vectorstore.persist()
+  return True
+
+# #################################
+# 対話するためのAPI
+# usage: curl -X POST -H 'Content-type: application/json' --data '{"query":"かんた ん決済について教えて"}' 'http://127.0.0.1:8000/chat'
+class Chat(BaseModel):
+  query: str
+
+chat_history = []
+@api.post("/chat")
+def chat_query(chat: Chat):
+  print(f"text: {chat.query}")
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+  llm = OpenAI(temperature=0,model_name=model_name,
+    max_tokens=512,
+    frequency_penalty=0.02
+  )
+  query = chat.query
+
+  # load from disk
+  embeddings = OpenAIEmbeddings()
+  vectorstore= Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+  # docs = vectorstore.similarity_search(query)
+  # vectorstore.persist()
+  pdf_qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever(), return_source_documents=True)
+  result = pdf_qa({"question": query, "chat_history": chat_history})
+  return result["answer"]
+
+# 参考：https://python.langchain.com/docs/integrations/vectorstores/chroma
---
+++ streamlit_langchain.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import streamlit.components.v1 as stc
+from streamlit_chat import message
+
+if "generated" not in st.session_state:
+    st.session_state.generated = []
+if "past" not in st.session_state:
+    st.session_state.past = []
+
+st.title("LangChain検証")
+
+from langchain import OpenAI, ConversationChain
+import openai
+import os
+
+import platform
+import openai
+import chromadb
+import langchain
+from langchain.llms import OpenAI
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+
+import json
+import requests
+
+import tempfile
+from pathlib import Path
+import base64
+
+
+# 参考：https://shunyaueta.com/posts/2021-07-08/
+def show_pdf(file_path:str):
+    """Show the PDF in Streamlit
+    That returns as html component
+
+    Parameters
+    ----------
+    file_path : [str]
+        Uploaded PDF file path
+    """
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+st.markdown('### PDFの内容を回答してくれるアプリ')
+
+st.markdown('#### 読み込みたいPDFをアップロード')
+file = st.file_uploader('画像をアップロードしてください.', type=['pdf'])
+if file:
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        # st.markdown("## Original PDF file")
+        pdftmpfile = f'{tmp_file.name}.pdf'
+        fp = Path(pdftmpfile)
+        fp.write_bytes(file.getvalue())
+        response = requests.post('http://127.0.0.1:8000/pdf', files={'file': open(pdftmpfile, 'rb')})
+        
+        st.markdown(f'{tmp_file.name} をアップロードしました.')
+
+pdf_url = ""
+chat_history = []
+pdf_url_reqistory = ""
+
+st.markdown('#### 対話')
+with st.form("my_forms"):
+    user_message = st.text_area("メッセージを送信してください")
+    submitted = st.form_submit_button("送信する")
+
+    if submitted:
+        json_data = {
+            "query" : user_message
+        }
+        st.markdown(json_data)
+        st.session_state.past.append(user_message)
+        response = requests.post('http://127.0.0.1:8000/chat', data = json.dumps(json_data) )
+        st.session_state.generated.append(response)
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state['past'][i], is_user=True, key=str(i) + "_user")
+            response = st.session_state['generated'][i]
+            if response is not None:
+                try:
+                    json_data = response.json()
+                    message(json_data, key=str(i))
+                except Exception as e:
+                    st.error(f"Failed to convert response to JSON: {str(e)}")
+
+stc.html('<div style="text-align: right;"> created by josys lab ai-1 </div>')
